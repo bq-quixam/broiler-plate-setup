d6c62a0164e3e33819915b980def23f7d367c5fc
rakesh <rakeshuvsn@users.noreply.github.com>
2017-10-07 17:11:32 -0400

    initial yoman setup is done for angular projects

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name broilerPlateSetupApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the broilerPlateSetupApp
+ */
+angular.module('broilerPlateSetupApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name broilerPlateSetupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the broilerPlateSetupApp
+ */
+angular.module('broilerPlateSetupApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
